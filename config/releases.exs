5d604ccf4445d071fc21fb21e3592fd798434327
Eduardo Borsa <borsa.dado@gmail.com>
2021-02-14 18:07:13 -0300

    updated stuff

--- config/releases.exs
+++ config/releases.exs
@@ -7,7 +7,7 @@ import Config
 config :deploy_tuto, DeployTuto.Repo,
   username: "postgres",
   password: "27q2s77ykCbVhfr5",
-  database: "deploy_tuto_dev",
+  database: "deploy_tuto",
   hostname: "localhost",
   ssl: true,
   show_sensitive_data_on_connection_error: true,
@@ -16,10 +16,9 @@ config :deploy_tuto, DeployTuto.Repo,
 secret_key_base = "2OmaRHgIu26E0cEpOsHlx8hgz6nlI5DCYT1dopYVPW0l7jQngkAhQS/XcB+i+RHu"
 
 config :deploy_tuto, DeployTutoWeb.Endpoint,
+  server: true,
+  secret_key_base: secret_key_base,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: 4000,
     transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
-
-config :deploy_tuto, DeployTutoWeb.Endpoint, server: true
+  ]
